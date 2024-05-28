2c30ee11cc9376b597f4a1fc151c34b9e9775ff6
“[firstname <rimshamoin07@gmail.com>
2024-05-28 04:26:54 -0700

    first commit

---
+++ main.js
@@ -0,0 +1,9 @@
+//Day 1 of 100 Days of CODING.
+//Question # 2 (Store a person’s name in a variable and print a message to them, like “Hello Asharib, would you like to learn some TypeScript today?”)
+var myname = "Rimsha"; // This saves the name
+console.log("Hello ".concat("Rimsha", ", would you like to learn some TypeScript today?")); // This shows the message
+//question number 3... (Store a person’s name in a variable, and then print that person’s name in lowercase, uppercase, and titlecase.)
+var namee = "rimsha";
+console.log(namee.toLowerCase());
+console.log(namee.toUpperCase());
+console.log(namee.charAt(0).toUpperCase() + namee.slice(1).toLowerCase());
---
+++ main.ts
@@ -0,0 +1,15 @@
+//Day 1 of 100 Days of CODING.
+
+
+//Question # 2 (Store a person’s name in a variable and print a message to them, like “Hello Asharib, would you like to learn some TypeScript today?”)
+
+let myname: string = "Rimsha"; // This saves the name
+
+console.log(`Hello ${"Rimsha"}, would you like to learn some TypeScript today?`); // This shows the message
+
+//question number 3... (Store a person’s name in a variable, and then print that person’s name in lowercase, uppercase, and titlecase.)
+
+let namee: string = "rimsha"
+console.log (namee.toLowerCase())
+console.log(namee.toUpperCase())
+console.log(namee.charAt(0).toUpperCase()+ namee.slice (1).toLowerCase())
